655ea90d6be914f8a383925620e86aa6ec80e472
cagiraldoa <cagiraldoa@eaft.edu.co>
2021-11-01 18:55:56 -0500

    Taller 2-3

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ $router->get('/', function () use ($router) {
 
 $router->get('/random-images', [
 
-    'as' => 'index', 'uses' => 'Controller@index'
+    'as' => 'r', 'uses' => 'Controller@index'
     
     ]);
